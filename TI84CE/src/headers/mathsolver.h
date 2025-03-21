5e92b5e9baab294046bd1d7328c068b6960308b4
Paulo Santos <paul.st.smith@gmail.com>
2025-03-21 05:42:51 -0300

    Enhance UI header file with detailed documentation for constants and function prototypes

--- TI84CE/src/headers/mathsolver.h
+++ TI84CE/src/headers/mathsolver.h
@@ -11,75 +11,114 @@
 
 /* ============================== Constants ============================== */
 
-#define MAX_INPUT_LENGTH     100   // Maximum input expression length
-#define MAX_NODES            50    // Maximum nodes in the expression tree
-#define MAX_TOKEN_LENGTH     20    // Maximum token length
-#define MAX_VARIABLES        10    // Maximum number of variables
-#define MAX_STEPS            20    // Maximum calculation steps to display
-#define SCREEN_WIDTH         320   // Calculator screen width
-#define SCREEN_HEIGHT        240   // Calculator screen height
-#define EPSILON              1e-10 // Small value for comparisons
+/** Maximum input expression length */
+#define MAX_INPUT_LENGTH     100
+
+/** Maximum nodes in the expression tree */
+#define MAX_NODES            50
+
+/** Maximum token length */
+#define MAX_TOKEN_LENGTH     20
+
+/** Maximum number of variables */
+#define MAX_VARIABLES        10
+
+/** Maximum calculation steps to display */
+#define MAX_STEPS            20
+
+/** Calculator screen width */
+#define SCREEN_WIDTH         320
+
+/** Calculator screen height */
+#define SCREEN_HEIGHT        240
+
+/** Small value for comparisons */
+#define EPSILON              1e-10
 
 /* Mathematical constants */
+/** Value of Pi */
 #define PI  3.14159265358979323846
+
+/** Base of natural logarithm (e) */
 #define E   2.71828182845904523536
+
+/** Golden ratio (phi) */
 #define PHI 1.61803398874989484820
 
 /* Colors */
-#define COLOR_BACKGROUND     0xFF   // White
-#define COLOR_TEXT           0x00   // Black
-#define COLOR_HIGHLIGHT      0x03   // Green
-#define COLOR_ERROR          0xE0   // Red
-#define COLOR_GRIDLINE       0xB5   // Light gray
+/** Background color (white) */
+#define COLOR_BACKGROUND     0xFF
 
-/* Token types */
+/** Text color (black) */
+#define COLOR_TEXT           0x00
+
+/** Highlight color (green) */
+#define COLOR_HIGHLIGHT      0x03
+
+/** Error color (red) */
+#define COLOR_ERROR          0xE0
+
+/** Gridline color (light gray) */
+#define COLOR_GRIDLINE       0xB5
+
+/* ============================== Types ============================== */
+
+/**
+ * Enumeration of token types for parsing
+ */
 typedef enum {
-    TOKEN_NONE,
-    TOKEN_NUMBER,
-    TOKEN_VARIABLE,
-    TOKEN_PLUS,
-    TOKEN_MINUS,
-    TOKEN_MULTIPLY,
-    TOKEN_DIVIDE,
-    TOKEN_POWER,
-    TOKEN_LEFT_PAREN,
-    TOKEN_RIGHT_PAREN,
-    TOKEN_COMMA,
-    TOKEN_FACTORIAL,
-    TOKEN_FUNCTION,
-    TOKEN_END
+    TOKEN_NONE,         /**< No token */
+    TOKEN_NUMBER,       /**< Numeric token */
+    TOKEN_VARIABLE,     /**< Variable token */
+    TOKEN_PLUS,         /**< Plus operator */
+    TOKEN_MINUS,        /**< Minus operator */
+    TOKEN_MULTIPLY,     /**< Multiplication operator */
+    TOKEN_DIVIDE,       /**< Division operator */
+    TOKEN_POWER,        /**< Power operator */
+    TOKEN_LEFT_PAREN,   /**< Left parenthesis */
+    TOKEN_RIGHT_PAREN,  /**< Right parenthesis */
+    TOKEN_COMMA,        /**< Comma separator */
+    TOKEN_FACTORIAL,    /**< Factorial operator */
+    TOKEN_FUNCTION,     /**< Function token */
+    TOKEN_END           /**< End of input */
 } TokenType;
 
-/* Node types */
+/**
+ * Enumeration of node types for the abstract syntax tree (AST)
+ */
 typedef enum {
-    NODE_NUMBER,
-    NODE_VARIABLE,
-    NODE_ADDITION,
-    NODE_SUBTRACTION,
-    NODE_MULTIPLICATION,
-    NODE_DIVISION,
-    NODE_EXPONENT,
-    NODE_FUNCTION,
-    NODE_FACTORIAL,
-    NODE_PARENTHESIS
+    NODE_NUMBER,        /**< Number node */
+    NODE_VARIABLE,      /**< Variable node */
+    NODE_ADDITION,      /**< Addition node */
+    NODE_SUBTRACTION,   /**< Subtraction node */
+    NODE_MULTIPLICATION,/**< Multiplication node */
+    NODE_DIVISION,      /**< Division node */
+    NODE_EXPONENT,      /**< Exponentiation node */
+    NODE_FUNCTION,      /**< Function node */
+    NODE_FACTORIAL,     /**< Factorial node */
+    NODE_PARENTHESIS    /**< Parenthesis node */
 } NodeType;
 
-/* Function types */
+/**
+ * Enumeration of supported mathematical functions
+ */
 typedef enum {
-    FUNC_SIN,
-    FUNC_COS,
-    FUNC_TAN,
-    FUNC_LOG,
-    FUNC_LN,
-    FUNC_SQRT,
-    FUNC_NONE
+    FUNC_SIN,           /**< Sine function */
+    FUNC_COS,           /**< Cosine function */
+    FUNC_TAN,           /**< Tangent function */
+    FUNC_LOG,           /**< Logarithm (base 10) */
+    FUNC_LN,            /**< Natural logarithm */
+    FUNC_SQRT,          /**< Square root */
+    FUNC_NONE           /**< No function */
 } FunctionType;
 
-/* Arithmetic types for formatting */
+/**
+ * Enumeration of arithmetic formatting modes
+ */
 typedef enum {
-    ARITHMETIC_NORMAL,
-    ARITHMETIC_TRUNCATE,
-    ARITHMETIC_ROUND
+    ARITHMETIC_NORMAL,  /**< Normal formatting */
+    ARITHMETIC_TRUNCATE,/**< Truncate decimals */
+    ARITHMETIC_ROUND    /**< Round decimals */
 } ArithmeticType;
 
 /* ============================== Structures ============================== */
@@ -88,19 +127,19 @@ typedef enum {
  * Source position structure for error reporting
  */
 typedef struct {
-    int start;  // Start index in the input string
-    int end;    // End index in the input string
-    int line;   // Line number
-    int column; // Column number
+    int start;  /**< Start index in the input string */
+    int end;    /**< End index in the input string */
+    int line;   /**< Line number */
+    int column; /**< Column number */
 } SourcePosition;
 
 /**
  * Token structure representing a parsed token
  */
 typedef struct {
-    TokenType type;              // Type of token
-    char value[MAX_TOKEN_LENGTH]; // Token value as string
-    SourcePosition position;     // Position in the input
+    TokenType type;              /**< Type of token */
+    char value[MAX_TOKEN_LENGTH];/**< Token value as string */
+    SourcePosition position;     /**< Position in the input */
 } Token;
 
 /**
@@ -112,39 +151,27 @@ typedef struct ExpressionNode ExpressionNode;
  * Expression node structure (the building block of the AST)
  */
 struct ExpressionNode {
-    NodeType type;               // Type of node
-    SourcePosition position;     // Position in source
+    NodeType type;               /**< Type of node */
+    SourcePosition position;     /**< Position in source */
     
-    // Node specific data depends on type
     union {
-        // Number node
-        double number_value;
-        
-        // Variable node
+        double number_value;     /**< Value for number nodes */
         struct {
-            char name[MAX_TOKEN_LENGTH];
+            char name[MAX_TOKEN_LENGTH]; /**< Variable name */
         } variable;
-        
-        // Binary operation node (add, subtract, multiply, divide, power)
         struct {
-            ExpressionNode* left;
-            ExpressionNode* right;
+            ExpressionNode* left; /**< Left operand */
+            ExpressionNode* right;/**< Right operand */
         } binary_op;
-        
-        // Function node
         struct {
-            FunctionType func_type;
-            ExpressionNode* argument;
+            FunctionType func_type; /**< Function type */
+            ExpressionNode* argument; /**< Function argument */
         } function;
-        
-        // Factorial node
         struct {
-            ExpressionNode* expression;
+            ExpressionNode* expression; /**< Expression for factorial */
         } factorial;
-        
-        // Parenthesis node
         struct {
-            ExpressionNode* expression;
+            ExpressionNode* expression; /**< Expression in parentheses */
         } parenthesis;
     };
 };
@@ -153,75 +180,174 @@ struct ExpressionNode {
  * Tokenizer structure for parsing input
  */
 typedef struct {
-    const char* input;   // Input string
-    int position;        // Current position in the input
-    int line;            // Current line number
-    int column;          // Current column number
-    Token current_token; // Current token
+    const char* input;   /**< Input string */
+    int position;        /**< Current position in the input */
+    int line;            /**< Current line number */
+    int column;          /**< Current column number */
+    Token current_token; /**< Current token */
 } Tokenizer;
 
 /**
  * Variable structure to store variable name and value
  */
 typedef struct {
-    char name[MAX_TOKEN_LENGTH];
-    double value;
-    bool is_defined;
+    char name[MAX_TOKEN_LENGTH]; /**< Variable name */
+    double value;                /**< Variable value */
+    bool is_defined;             /**< Whether the variable is defined */
 } Variable;
 
 /**
  * Calculation step structure for step-by-step output
  */
 typedef struct {
-    char expression[MAX_INPUT_LENGTH];
-    char operation[MAX_INPUT_LENGTH];
-    char result[MAX_INPUT_LENGTH];
+    char expression[MAX_INPUT_LENGTH]; /**< Expression at this step */
+    char operation[MAX_INPUT_LENGTH];  /**< Operation performed */
+    char result[MAX_INPUT_LENGTH];     /**< Result of the operation */
 } CalculationStep;
 
 /**
  * Calculation result structure
  */
 typedef struct {
-    double value;                    // Final value
-    int step_count;                  // Number of steps
-    CalculationStep steps[MAX_STEPS]; // Step-by-step calculation
-    char formatted_result[MAX_TOKEN_LENGTH]; // Formatted result string
-    ArithmeticType arithmetic_mode;  // Arithmetic mode used
-    int precision;                   // Precision used
-    bool use_significant_digits;     // Whether significant digits were used
+    double value;                    /**< Final value */
+    int step_count;                  /**< Number of steps */
+    CalculationStep steps[MAX_STEPS];/**< Step-by-step calculation */
+    char formatted_result[MAX_TOKEN_LENGTH]; /**< Formatted result string */
+    ArithmeticType arithmetic_mode;  /**< Arithmetic mode used */
+    int precision;                   /**< Precision used */
+    bool use_significant_digits;     /**< Whether significant digits were used */
 } CalculationResult;
 
 /* ============================== Function Prototypes ============================== */
 
-// Initialization and cleanup
+/**
+ * Initialize the MathSolver library
+ */
 void mathsolver_init(void);
+
+/**
+ * Clean up resources used by the MathSolver library
+ */
 void mathsolver_cleanup(void);
 
-// Tokenization and parsing
+/**
+ * Initialize the tokenizer with the given input string
+ * @param tokenizer Pointer to the tokenizer structure
+ * @param input Input string to tokenize
+ */
 void tokenizer_init(Tokenizer* tokenizer, const char* input);
+
+/**
+ * Get the next token from the tokenizer
+ * @param tokenizer Pointer to the tokenizer structure
+ * @return The next token
+ */
 Token get_next_token(Tokenizer* tokenizer);
+
+/**
+ * Parse an input string into an expression tree
+ * @param input Input string to parse
+ * @return Pointer to the root of the expression tree
+ */
 ExpressionNode* parse_expression_string(const char* input);
+
+/**
+ * Parse an expression from the tokenizer
+ * @param tokenizer Pointer to the tokenizer structure
+ * @return Pointer to the root of the parsed expression tree
+ */
 static ExpressionNode* parse_expression(Tokenizer* tokenizer);
 
-// Evaluation
+/**
+ * Evaluate an expression string and store the result
+ * @param input Input string to evaluate
+ * @param result Pointer to the result structure
+ * @return True if evaluation was successful, false otherwise
+ */
 bool evaluate_expression_string(const char* input, CalculationResult* result);
+
+/**
+ * Evaluate an expression tree
+ * @param node Pointer to the root of the expression tree
+ * @return The evaluated result
+ */
 double evaluate_expression(ExpressionNode* node);
+
+/**
+ * Evaluate an expression tree with step-by-step output
+ * @param node Pointer to the root of the expression tree
+ * @param result Pointer to the result structure
+ * @return The evaluated result
+ */
 double evaluate_with_steps(ExpressionNode* node, CalculationResult* result);
 
-// Variable management
+/**
+ * Set the value of a variable
+ * @param name Name of the variable
+ * @param value Value to assign to the variable
+ */
 void set_variable(const char* name, double value);
+
+/**
+ * Get the value of a variable
+ * @param name Name of the variable
+ * @param found Pointer to a boolean indicating if the variable was found
+ * @return The value of the variable
+ */
 double get_variable(const char* name, bool* found);
+
+/**
+ * Check if a name corresponds to a mathematical constant
+ * @param name Name to check
+ * @return True if the name is a constant, false otherwise
+ */
 bool is_constant(const char* name);
 
-// Formatting
+/**
+ * Format a number into a string
+ * @param value Number to format
+ * @param buffer Buffer to store the formatted string
+ */
 void format_number(double value, char* buffer);
+
+/**
+ * Format an expression tree into a string
+ * @param node Pointer to the root of the expression tree
+ * @param buffer Buffer to store the formatted string
+ */
 void format_expression(ExpressionNode* node, char* buffer);
+
+/**
+ * Apply arithmetic formatting to a number
+ * @param value Number to format
+ * @return The formatted number
+ */
 double apply_arithmetic_format(double value);
 
-// Settings
+/**
+ * Set the arithmetic mode and precision
+ * @param type Arithmetic mode to set
+ * @param precision Number of decimal places or significant digits
+ * @param use_significant digits Whether to use significant digits
+ */
 void set_arithmetic_mode(ArithmeticType type, int precision, bool use_significant_digits);
+
+/**
+ * Get the current arithmetic mode
+ * @return The current arithmetic mode
+ */
 ArithmeticType get_arithmetic_mode(void);
+
+/**
+ * Get the current precision setting
+ * @return The current precision
+ */
 int get_precision(void);
+
+/**
+ * Check if significant digits are being used
+ * @return True if significant digits are used, false otherwise
+ */
 bool get_use_significant_digits(void);
 
 #endif /* MATHSOLVER_H */
--- TI84CE/src/headers/ui.h
+++ TI84CE/src/headers/ui.h
@@ -1,42 +1,107 @@
 /**
  * MathSolver for TI-84 CE - Text-based UI Definitions
  * 
- * User interface functions for the math solver using ti/screen.h
+ * This header file defines constants and function prototypes for the 
+ * text-based user interface of the MathSolver application. It provides 
+ * functions for displaying UI components, handling user input, and 
+ * managing settings.
  */
 
- #ifndef UI_H
- #define UI_H
- 
- #include "mathsolver.h"
- 
- /* ============================== UI Constants ============================== */
- 
- #define MAX_DISPLAY_ROWS     10    // Maximum number of rows to display
- #define MAX_DISPLAY_COLS     26    // Maximum number of columns to display
- #define MAX_MENU_ITEMS       5     // Maximum number of menu items
- #define CURSOR_BLINK_RATE    10    // Number of frames between cursor blinks
- 
- /* ============================== Function Prototypes ============================== */
- 
- // Main UI function
- void run_calculator_ui(void);
- 
- // UI components
- void draw_header(void);
- void show_input_prompt(void);
- void show_calculation_result(CalculationResult* result);
- void show_error_message(const char* message);
- void show_help_screen(void);
- void show_settings_menu(void);
- 
- // Input handling
- bool get_expression_input(char* buffer, int buffer_size);
- void handle_keys(void);
- 
- // Settings
- void toggle_arithmetic_mode(void);
- void adjust_precision(int change);
- void toggle_significant_digits(void);
- 
- #endif /* UI_H */
- 
+#ifndef UI_H
+#define UI_H
+
+#include "mathsolver.h"
+
+/* ============================== UI Constants ============================== */
+
+/** Maximum number of rows that can be displayed on the screen. */
+#define MAX_DISPLAY_ROWS     10
+
+/** Maximum number of columns that can be displayed on the screen. */
+#define MAX_DISPLAY_COLS     26
+
+/** Maximum number of items that can be displayed in a menu. */
+#define MAX_MENU_ITEMS       5
+
+/** Number of frames between cursor blinks for visual feedback. */
+#define CURSOR_BLINK_RATE    10
+
+/* ============================== Function Prototypes ============================== */
+
+/**
+ * Runs the main calculator user interface loop.
+ */
+void run_calculator_ui(void);
+
+/* ------------------------------ UI Components ------------------------------ */
+
+/**
+ * Draws the header section of the UI.
+ */
+void draw_header(void);
+
+/**
+ * Displays the input prompt for the user to enter a mathematical expression.
+ */
+void show_input_prompt(void);
+
+/**
+ * Displays the result of a calculation.
+ * 
+ * @param result Pointer to the CalculationResult structure containing the result.
+ */
+void show_calculation_result(CalculationResult* result);
+
+/**
+ * Displays an error message on the screen.
+ * 
+ * @param message Pointer to the error message string.
+ */
+void show_error_message(const char* message);
+
+/**
+ * Displays the help screen with usage instructions.
+ */
+void show_help_screen(void);
+
+/**
+ * Displays the settings menu for user configuration.
+ */
+void show_settings_menu(void);
+
+/* ------------------------------ Input Handling ------------------------------ */
+
+/**
+ * Prompts the user to input a mathematical expression.
+ * 
+ * @param buffer Pointer to the buffer where the input will be stored.
+ * @param buffer_size Size of the buffer.
+ * @return True if input was successfully received, false otherwise.
+ */
+bool get_expression_input(char* buffer, int buffer_size);
+
+/**
+ * Handles key inputs from the user.
+ */
+void handle_keys(void);
+
+/* ------------------------------ Settings ------------------------------ */
+
+/**
+ * Toggles the arithmetic mode (e.g., basic or advanced).
+ */
+void toggle_arithmetic_mode(void);
+
+/**
+ * Adjusts the precision of calculations by a specified change.
+ * 
+ * @param change The amount to adjust the precision by.
+ */
+void adjust_precision(int change);
+
+/**
+ * Toggles the display of significant digits in results.
+ */
+void toggle_significant_digits(void);
+
+#endif /* UI_H */
